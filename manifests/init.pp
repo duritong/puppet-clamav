2e794c88d8378b968e2ca1a3da03e5b924e2a6dc
mh <mh@immerda.ch>
2008-03-09 21:15:20 +0000

    added dcc module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,8 @@
 # modules/clamav/manifests/init.pp - manage clamav stuff
 # Copyright (C) 2007 admin@immerda.ch
+# GPLv3
+# this module requires the amavisd-new module
+# this module is part of a whole bunch of modules, please have a look at the exim module
 #
 
 # modules_dir { "clamav": }
@@ -16,6 +19,7 @@ class clamav::base {
 
     package{clamav:
         ensure => installed,
+        require => Class[amavisd-new],
     }
 
     service{clamd:
