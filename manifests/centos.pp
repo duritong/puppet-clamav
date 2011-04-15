6ca30167a8ca748f9d48e7b827e6b98b7a598cde
mh <mh@immerda.ch>
2011-04-15 14:23:40 +0200

    update to the new package style

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -6,32 +6,8 @@ class clamav::centos inherits clamav::base {
     hasstatus => true
   }
 
-  file{'/etc/sysconfig/freshclam':
-    source => [ "puppet:///modules/site-clamav/sysconfig/${fqdn}/freshclam",
-                "puppet:///modules/site-clamav/sysconfig/freshclam",
-                "puppet:///modules/clamav/sysconfig/freshclam" ],
-    require => Package['clamav-update'],
-    owner => root, group => root, mode => 0644;
-  }
-
-  file{'/etc/cron.d/clamav-update':
-    source => [ "puppet:///modules/site-clamav/cron.d/${fqdn}/clamav-update",
-                "puppet:///modules/site-clamav/cron.d/clamav-update",
-                "puppet:///modules/clamav/cron.d/clamav-update" ],
-    require => Package['clamav-update'],
-    owner => root, group => root, mode => 0600;
-  }
-
-  package{'clamav-update':
-    ensure => present,
-  }
-
   File['/etc/clamd.conf']{
     source => undef,
     ensure => absent
-  }
-
-  File['/etc/freshclam.conf']{
-    require +> Package['clamav-update']
   }
 }
