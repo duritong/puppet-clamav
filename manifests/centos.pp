2baaf235de307c20fb8e7da09a378a5f73fcf2dc
mh <mh@immerda.ch>
2011-07-17 21:09:44 +0200

    fixing missing directory

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -1,9 +1,15 @@
 class clamav::centos inherits clamav::base {
   require amavisd-new
 
+  file{'/var/run/clamd.amavisd':
+    ensure => directory,
+    owner => amavis, group => 0, mode => 0700;
+  }
+
   Service['clamd']{
     name => 'clamd.amavisd',
-    hasstatus => true
+    hasstatus => true,
+    require => File['/var/run/clamd.amavisd'],
   }
 
   File['/etc/clamd.conf']{
