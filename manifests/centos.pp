911865a52c9b5986d1f5c7c86a3e35c3b8aae121
mh <mh@immerda.ch>
2014-08-24 12:19:50 +0200

    this is now done as part of the amavisd-new rpm

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -2,18 +2,9 @@
 class clamav::centos inherits clamav::base {
   require amavisd_new
 
-  file{'/var/run/clamd.amavisd':
-    ensure  => directory,
-    require => Package['amavisd-new'],
-    owner   => amavis,
-    group   => 0,
-    mode    => '0700';
-  }
-
   Service['clamd']{
     name      => 'clamd.amavisd',
     hasstatus => true,
-    require   => File['/var/run/clamd.amavisd'],
   }
 
   File['/etc/clamd.conf']{
