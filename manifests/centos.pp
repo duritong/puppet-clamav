5684ff97dfc7d4a18392a4b1cd2e5ff713f2cec0
mh <mh@immerda.ch>
2017-07-20 09:13:18 +0200

    this is not anymore needed

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -4,10 +4,6 @@ class clamav::centos inherits clamav::base {
     package{'clamav-update':
       ensure => present,
       before => File_line['enable_freshclam'],
-    } -> file_line{'enable_freshclam_update':
-      line  => '#FRESHCLAM_DELAY=disabled-warn   # REMOVE ME',
-      path  => '/etc/sysconfig/freshclam',
-      match => '^(#)?FRESHCLAM_DELAY=disabled-warn',
     }
     if str2bool($::selinux) {
       selboolean{
