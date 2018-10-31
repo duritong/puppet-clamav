f4da1d8a6ff606874a2c85bdcfb884f892495376
mh <mh@immerda.ch>
2018-10-31 18:37:38 +0100

    make sure we update clamav

--- manifests/centos.pp
+++ manifests/centos.pp
@@ -3,7 +3,9 @@ class clamav::centos inherits clamav::base {
   if versioncmp($::operatingsystemmajrelease,'6') > 0 {
     package{'clamav-update':
       ensure => present,
-      before => File_line['enable_freshclam'],
+    } ~> exec{'/usr/bin/freshclam --quiet':
+      refreshonly => true,
+      before      => File_line['enable_freshclam'],
     }
     if str2bool($::selinux) {
       selboolean{
